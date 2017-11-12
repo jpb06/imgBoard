34d7b542840b72b466169aac3bab824640762a26
jpb <jpb.06@outlook.com>
2017-11-12 20:41:36 +0100

    Main persistence (Business)

---
+++ ImgBoard.Business/Internal/PersistenceManager.cs
@@ -0,0 +1,22 @@
+﻿using ImgBoard.Business.Internal.Contracts;
+using ImgBoard.Dal.Manipulation.Services.Main.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgBoard.Business.Internal
+{
+    internal class PersistenceManager : IPersistenceManager
+    {
+        private IPersistenceService persistenceService;
+
+        public PersistenceManager(IPersistenceService persistenceService)
+        {
+            this.persistenceService = persistenceService;
+        }
+
+        
+    }
+}
--- ImgBoard.Business/InversionOfControl/IoCConfiguration.cs
+++ ImgBoard.Business/InversionOfControl/IoCConfiguration.cs
@@ -4,8 +4,11 @@ using ImgBoard.Dal.Context.Contracts;
 using ImgBoard.Dal.Context.EndObjects;
 using ImgBoard.Dal.Manipulation.Repositories.Contracts;
 using ImgBoard.Dal.Manipulation.Repositories.Implementation.Base;
+using ImgBoard.Dal.Manipulation.Repositories.Implementation.Specific;
 using ImgBoard.Dal.Manipulation.Services.ErrorsReporting;
 using ImgBoard.Dal.Manipulation.Services.ErrorsReporting.Contracts;
+using ImgBoard.Dal.Manipulation.Services.Main;
+using ImgBoard.Dal.Manipulation.Services.Main.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +28,7 @@ namespace ImgBoard.Business.InversionOfControl
         {
             container = new UnityContainer();
 
+            #region Errors reporting
             container.RegisterType<IErrorsReportingContext, ErrorsReportingContext>(
                 new HierarchicalLifetimeManager()
             );
@@ -34,6 +38,22 @@ namespace ImgBoard.Business.InversionOfControl
             container.RegisterType<IErrorsReportingService, ErrorsReportingService>();
 
             container.RegisterType<IErrorsReportingManager, ErrorsReportingManager>();
+            #endregion
+
+            #region Main
+            container.RegisterType<IImgBoardContext, ImgBoardTestContext>(new HierarchicalLifetimeManager());
+
+            container.RegisterType<ICategoryRepository, CategoryRepository>();
+            container.RegisterType<ICommentRepository, CommentRepository>();
+            container.RegisterType<IImageRepository, ImageRepository>();
+            container.RegisterType<ITagRepository, TagRepository>();
+            container.RegisterType<ICategoryRepository, CategoryRepository>();
+            container.RegisterType<IUserRepository, UserRepository>();
+
+            container.RegisterType<IPersistenceService, PersistenceService>();
+
+            container.RegisterType<IErrorsReportingManager, ErrorsReportingManager>();
+            #endregion
         }
     }
 }
