0bcf41cdad8c45c3ba613e8a3e03c7336564e7de
jpb <jpb.06@outlook.com>
2017-11-13 20:44:14 +0100

    Navbar selected item feature

---
+++ ImgBoard.Web/Controllers/ImagesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImgBoard.Web.Controllers
+{
+    public class ImagesController : Controller
+    {
+        // GET: Images
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ByCategory()
+        {
+            return View();
+        }
+
+        public ActionResult ByTag()
+        {
+            return View();
+        }
+    }
+}
