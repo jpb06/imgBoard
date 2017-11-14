96a84bba84923ad48db64606b7e510e2bc9b93c8
jpb <jpb.06@outlook.com>
2017-11-14 17:30:15 +0100

    Basic design for the images page

--- ImgBoard.Web/Code/Extensions/HtmlHelperExtensions.cs
+++ ImgBoard.Web/Code/Extensions/HtmlHelperExtensions.cs
@@ -27,5 +27,26 @@ namespace ImgBoard.Web.Code.Extensions
             );
             return new MvcHtmlString(str);
         }
+
+        public static MvcHtmlString ActionImage(this HtmlHelper html, string action, string controllerName, object routeValues, string imagePath, string alt = "", string imageClass = "")
+        {
+            var url = new UrlHelper(html.ViewContext.RequestContext);
+
+            // build the <img> tag
+            var imgBuilder = new TagBuilder("img");
+            imgBuilder.MergeAttribute("src", url.Content(imagePath));
+            imgBuilder.MergeAttribute("alt", alt);
+            imgBuilder.MergeAttribute("class", imageClass);
+            string imgHtml = imgBuilder.ToString(TagRenderMode.SelfClosing);
+
+            // build the <a> tag
+            var anchorBuilder = new TagBuilder("a");
+
+            anchorBuilder.MergeAttribute("href", url.Action(action, controllerName, routeValues));
+            anchorBuilder.InnerHtml = imgHtml; // include the <img> tag inside
+            string anchorHtml = anchorBuilder.ToString(TagRenderMode.Normal);
+
+            return MvcHtmlString.Create(anchorHtml);
+        }
     }
 }
