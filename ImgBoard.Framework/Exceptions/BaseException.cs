cc03379a6496b0f17d3c24613221d62b382fae3e
jpb <jpb.06@outlook.com>
2017-11-17 03:13:05 +0100

    Base exception

---
+++ ImgBoard.Framework/Exceptions/BaseException.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace ImgBoard.Framework.Exceptions
+{
+    public class BaseException : Exception
+    {
+        public string errorType;
+
+        public BaseException(string errorType, string message) : base(message)
+        {
+            this.errorType = errorType;
+        }
+    }
+}
