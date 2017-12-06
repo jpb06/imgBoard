4a3c5da55b080a284e3685392f72c4da72a720c4
jpb <jpb.06@outlook.com>
2017-12-06 03:35:32 +0100

    Moving site api controller to another folder for clarity

---
+++ ImgBoard.Web/Api/SiteApiController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using ImgBoard.Models.Main;
+using ImgBoard.Business.Exposed;
+using System.Threading.Tasks;
+
+namespace ImgBoard.Web.Api
+{
+    public class SiteApiController : ApiController
+    {
+        [HttpGet]
+        [Route("siteapi/getimages")]
+        public async Task<IEnumerable<Image>> GetImages()
+        {
+            return await Images.GetAsync();
+        }
+
+        [HttpGet]
+        [Route("siteapi/getimagesmatchingcategory/{term}")]
+        public async Task<IEnumerable<Image>> GetImagesMatchingCategory(string term)
+        {
+            return await Images.GetMatchAsync(term);
+        }
+    }
+}
