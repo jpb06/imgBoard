10559df6ab28182065c5779b626a661e288d9753
jpb <jpb.06@outlook.com>
2017-12-15 15:08:25 +0100

    Extended search progress

--- ImgBoard.Web/TypeScript/ViewModels/BoardViewModel.ts
+++ ImgBoard.Web/TypeScript/ViewModels/BoardViewModel.ts
@@ -1,25 +1,28 @@
 ﻿namespace ViewModels {
     export class BoardViewModel {
-        LastFetchedTerm: string;
-
         Action: KnockoutObservable<string>;
         Images: KnockoutObservableArray<FrontModels.Image>;
         SearchText: KnockoutObservable<string>;
         Matches: KnockoutObservableArray<FrontModels.Category>;
-
+        
         Term: KnockoutComputed<string>;
+
+        SearchSettings: KnockoutObservableArray<InternalModels.SearchSetting>;
 
         Search: () => void;
         FetchImagesByMatch: (match: FrontModels.Category) => void;
+        RemoveSearchSetting: (searchSetting: InternalModels.SearchSetting) => void;
 
         ApplySearchResult: (action: string, images: Array<Models.IImage>, timeStart: number) => void;
+        MatchesSearch: (term: string) => void;
 
         constructor(images: Array<Models.IImage>) {
-            this.LastFetchedTerm = "";
             this.Action = ko.observable("All images");
             this.SearchText = ko.observable("");
             this.Images = ko.observableArray(images.map(el => new FrontModels.Image(el)));
             this.Matches = ko.observableArray([]);
+
+            this.SearchSettings = ko.observableArray([]);
 
             this.Term = ko.pureComputed<string>(this.SearchText)
                 .extend({ rateLimit: { method: "notifyWhenChangesStop", timeout: 400 } });
@@ -28,12 +31,7 @@
                 if (term.length === 0) {
                     $('#search-matches').hide();
                 } else {
-                    if (this.LastFetchedTerm !== term) {
-                        ApiRequests.GetMatchingCategories(term, categories => {
-                            this.Matches(categories.map(el => new FrontModels.Category(el)));
-                            $('#search-matches').show();
-                        });
-                    }
+                    this.MatchesSearch(term);
                 }
             }, this);
 
@@ -50,20 +48,20 @@
                         this.ApplySearchResult("All images", images, timeStart);
                     });
                 } else {
-                    if (this.LastFetchedTerm !== term) {
-                        ApiRequests.GetMatchingCategories(term, categories => {
-                            this.Matches(categories.map(el => new FrontModels.Category(el)));
-                            $('#search-matches').show();
-                        });
-                    }
+                    this.MatchesSearch(term);
                 }
             };
 
             this.FetchImagesByMatch = (match: FrontModels.Category) => {
                 let timeStart = Date.now();
+                
+                this.SearchText("");
 
-                this.SearchText(match.Title);
-                this.LastFetchedTerm = match.Title;
+                this.SearchSettings.push({
+                    Id: match.Id,
+                    Type: "Category",
+                    Value: match.Title
+                });
 
                 $('#search-matches').hide();
                 $('#main-throbber').show();
@@ -72,6 +70,10 @@
                 ApiRequests.GetImagesByCategory(match.Id, images => {
                     this.ApplySearchResult("Matches for : " + match.Title, images, timeStart);
                 });
+            };
+
+            this.RemoveSearchSetting = (searchSetting: InternalModels.SearchSetting) => {
+                this.SearchSettings.remove(searchSetting);
             };
 
             this.ApplySearchResult = (
@@ -83,6 +85,24 @@
                 this.Images(images.map(el => new FrontModels.Image(el)));
                 Util.SetMinimumTimeout(timeStart, 500, ViewsCode.ResetBoardDom);
             };
+
+            this.MatchesSearch = (term: string) => {
+                ApiRequests.GetMatchingCategories(term, categories => {
+                    let result = categories
+                        .map(el => new FrontModels.Category(el))
+                        .filter(el => {
+
+                            let existing = ko.utils.arrayFirst(this.SearchSettings(), (item: InternalModels.SearchSetting) => {
+                                return item.Value === el.Title;
+                            });
+
+                            return existing === null;
+                        });
+
+                    this.Matches(result);
+                    $('#search-matches').show();
+                });
+            }
         }
     }
 }
