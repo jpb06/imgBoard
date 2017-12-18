75d0418dedd9e79fb288a538329690bec4cbc2ce
jpb <jpb.06@outlook.com>
2017-12-18 06:02:16 +0100

    Modifying site Api to make use of the new exposed method in business layer

---
+++ ImgBoard.Models/Api/GetImagesParameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgBoard.Models.Api
+{
+    public class GetImagesParameters
+    {
+        public int[] CategoriesIds { get; set; }
+        public int[] TagsIds { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Uploader { get; set; }
+        public string Extension { get; set; }
+    }
+}
